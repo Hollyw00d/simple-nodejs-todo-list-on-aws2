af3c4a7e143f4b9aeb2361899ee1cb2867586403
Hollyw00d <matt@mattjennings.net>
2018-02-25 13:42:58 -0800

    created page for login

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// An instance of the express app
+const app = express();
+
+// bodyParser can send post data
+app.use(bodyParser.urlencoded( {extended: true} ));
+
+// Tell express to use handlebars as it's templating engine
+// and look for a file called "views/layouts/base.handlebars" for
+// the master layouts and then other files inside "views"
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+// Set port
+let port = Number(process.env.PORT || 3000);
+
+// app listens locally on port 3000
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
